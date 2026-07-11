ebb5deffacf37dcc8dc73299975a8041a76172ab
OM BHARGAV <ombhargav788@gmail.com>
2026-07-12 00:28:47 +0530

    fixed logo

--- main-app/components/global/Navbar.tsx
+++ main-app/components/global/Navbar.tsx
@@ -2,13 +2,16 @@ import { SITE_NAME } from '@/config'
 import { MessageCircle } from 'lucide-react'
 import { Button } from '../ui/button'
 import Link from "next/link";
+import Image from 'next/image';
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 border-b bg-background/80 backdrop-blur">
       <div className="max-w-7xl mx-auto flex h-16 items-center justify-between px-4">
         <Link href={"/"}>
         <div className="flex items-center gap-3">
-          <MessageCircle className="h-6 w-6 text-primary" />
+          <div className='relative h-8 w-10'>
+          <Image src={"/logo.png"} fill alt={"Logo"}/>
+          </div>
           <span className="text-xl font-bold">
             {SITE_NAME}
           </span>
