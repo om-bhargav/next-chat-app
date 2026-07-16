94abe97affb220b67ffcaa1a31cbe2030ce52917
OM BHARGAV <ombhargav788@gmail.com>
2026-07-16 13:21:20 +0530

    fixed commit

--- main-app/components/providers/ThemeProvider.tsx
+++ main-app/components/providers/ThemeProvider.tsx
@@ -1,26 +1,22 @@
 "use client";
 
-import { useEffect } from "react";
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({
-    children,
-    ...props
+  children,
+  ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-    const { setTheme } = useTheme();
-    useEffect(() => {
-        setTheme("dark");
-    }, []);
-    return (
-        <NextThemesProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-            {...props}
-        >
-            {children}
-        </NextThemesProvider>
-    );
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      forcedTheme="dark"
+      enableSystem={false}
+      disableTransitionOnChange
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  );
 }
